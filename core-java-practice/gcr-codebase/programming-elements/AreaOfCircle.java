3ab98df4a0f72633d68991b3cd1f8d9163b9ddb3
Priyanshucs22 <priyanshusharma0156@gmail.com>
2025-12-17 15:51:49 +0530

    [Priyanshu sharma] added 4 basic problems of java

---
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double r = 7;
+        double area = 3.14 * r * r;
+        System.out.println("Area = " + area);
+    }
+}
